77ddcb1a6e753d00ff3446335a7c3ec422675b87
ShinRai <shinrai1090@gmail.com>
2023-01-19 23:22:17 +0530

    Added js logic

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,25 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(expenseProps) {
-  const {date: expenseDate, title: expenseTitle, amount: expenseAmount} = expenseProps.expenseData;
+  const {
+    date: expenseDate,
+    title: expenseTitle,
+    amount: expenseAmount,
+  } = expenseProps.expenseData;
+
+  const month = expenseDate.toLocaleString('en-US', {month: 'long'});
+  const day = expenseDate.toLocaleString('en-US', {day: '2-digit'});
+  const year = expenseDate.getFullYear();
 
   /* return <h2>Expense Item!</h2> */
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+        {/* {expenseDate.toISOString()} */}
+      </div>
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
